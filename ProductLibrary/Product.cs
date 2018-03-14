b131a41b7239ca79aa1b7e7d22b26762d37f26e7
Crone <crone1992@gmail.com>
2018-03-14 11:59:10 +0100

    Created 2 classes for library

--- ProductLibrary/Product.cs
+++ ProductLibrary/Product.cs
@@ -8,5 +8,17 @@ namespace ProductLibrary
 {
     public class Product
     {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+        public Category Category { get; set; }
+
+        public Product(string name, double price, int amount, Category cat)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+            Category = cat;
+        }
     }
 }
